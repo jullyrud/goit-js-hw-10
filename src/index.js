f89a2c645fbc96f7af87ee900aadb38955eeda63
jullyrud <94559083+jullyrud@users.noreply.github.com>
2022-12-11 08:22:48 +0100

    2nd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries.js';
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box')
+}
+
+refs.input.addEventListener('input', debounce(onInputActions, 1500))
+
+
+function onInputActions(e) {
+    const inputVal = e.target.value.trim()
+    console.log(inputVal);
+    
+    if (inputVal === '' ) {
+        return alert('запоните поле')
+    }
+
+    fetchCountries(inputVal).then(data => {
+        if (data.length > 9) {
+        alert("Too many matches found. Please enter a more specific name.")
+        }   console.log(data);;
+    })
+    
+    
+    
+    
+}
